c95611f9515c3c6f6ece6688e9902786aebc3435
winkidney <winkidney@gmail.com>
2015-06-16 23:58:04 +0800

    login check.

--- storygear/sgear/urls.py
+++ storygear/sgear/urls.py
@@ -6,7 +6,7 @@ from storygear.sgear.views import StoryHomeView, SingleStoryView, NewStoryView, 
 urlpatterns = (
     url(r'^$', StoryHomeView(), name="story"),
     url(r'^(\d+)/$', SingleStoryView(), name="single_story"),
-    url(r'^(\d+)/edit/$', EditStoryView(), name="edit_story"),
-    url(r'^(\d+)/new_chapter/$', NewChapterView(), name="new_chapter"),
-    url(r'^new/$', NewStoryView(), name="new_story"),
+    url(r'^(\d+)/edit/$', EditStoryView(decorators=login_required(login_url="/accounts/login/")), name="edit_story"),
+    url(r'^(\d+)/new_chapter/$', NewChapterView(decorators=login_required(login_url="/accounts/login/")), name="new_chapter"),
+    url(r'^new/$', NewStoryView(decorators=login_required(login_url="/accounts/login/")), name="new_story"),
 )
